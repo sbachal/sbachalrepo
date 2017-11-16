38ab35cff75b7adb577d927fd39da3b77b8ff6b2
Sagar Bachal <Sagar@Sagars-MacBook-Air.local>
2017-11-16 20:20:58 +0530

    Removed Comment

--- HashMap123456 copy.java
+++ HashMap123456 copy.java
@@ -6,7 +6,7 @@ public class HashMap
 
 	public static void main(String[] args) 
 	{
-		// TODO Auto-generated method stub
+		
 		 HashMap<Integer, String> hmap = new HashMap<Integer, String>();
 
 	      /*Adding elements to HashMap*/
